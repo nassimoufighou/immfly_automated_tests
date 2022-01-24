00b60dd402eee85f4a80f2f7c71b87c834e3cceb
Nassim <ounassim@gmail.com>
2022-01-24 17:57:52 +0100

    Update Request and RequestExecutor classes. Delete unused class.

--- src/main/java/core/request/Request.java
+++ src/main/java/core/request/Request.java
@@ -1,29 +1,18 @@
 package core.request;
-
-import org.json.simple.JSONObject;
-
-import java.util.HashMap;
-
 public abstract class Request {
 
     protected HttpMethod method;
-    protected String url;
-    protected JSONObject body = new JSONObject();
-    protected String host;
+    protected String body;
     protected String endpoint;
     protected String authorizationToken;
-    protected String bodyParameters;
-    protected HashMap<String, String> urlParameters =  new HashMap<>();
 
     public HttpMethod getMethod() { return  method; }
-
-    public String getUrl() { return url; }
 
     public String getEndpoint() {
         return endpoint;
     }
 
-    public JSONObject getBody() { return body; }
+    public String getBody() { return body; }
 
     public boolean hasToken() { return  authorizationToken != null && !authorizationToken.isEmpty(); }
 
--- src/main/java/core/request/RequestExecutor.java
+++ src/main/java/core/request/RequestExecutor.java
@@ -8,10 +8,11 @@ import core.reporting.Report;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-
 import static io.restassured.RestAssured.given;
 
 public class RequestExecutor {
+
+    private static Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     public static Response execute(Request request) {
         RestAssured.baseURI = APIConfig.getBaseUri();
@@ -23,16 +24,51 @@ public class RequestExecutor {
         switch (request.getMethod()) {
             case GET:
                 response = requestSpecification.when().get(request.getEndpoint()).then().extract().response();
+                Report.logStepInReport(Status.PASS, "GET request with body: <code>" + gson.toJson(request.getBody()) + "</code>");
                 break;
             case POST:
-                response = requestSpecification.body(request.getBody().toString()).when().post(request.getEndpoint()).then().extract().response();
+                response = requestSpecification.body(request.getBody()).when().post(request.getEndpoint()).then().extract().response();
+                Report.logStepInReport(Status.PASS, "POST request with body: <code>" + gson.toJson(request.getBody()) + "</code>");
+                break;
+            case PUT:
+                response = requestSpecification.body(request.getBody()).when().put(request.getEndpoint()).then().extract().response();
+                Report.logStepInReport(Status.PASS, "PUT request with body: <code>" + gson.toJson(request.getBody()) + "</code>");
                 break;
             case DELETE:
                 response = requestSpecification.when().delete(request.getEndpoint()).then().extract().response();
+                Report.logStepInReport(Status.PASS, "DELETE request with body: <code>" + gson.toJson(request.getBody()) + "</code>");
                 break;
         }
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        Report.logStepInReport(Status.INFO, request.getMethod() + " response: <code>" + gson.toJson(response.getBody().asString() + "</code>"));
+        Report.logStepInReport(Status.PASS, request.getMethod() + " response: <code>" + gson.toJson(response.getBody().asString() + "</code>"));
+        return response;
+    }
+
+    public static Response executePreconditionRequest(Request request, String action) {
+        RestAssured.baseURI = APIConfig.getBaseUri();
+        Response response = null;
+        RequestSpecification requestSpecification = given();
+        requestSpecification.header("Content-type", "application/json");
+        if (request.hasToken()) requestSpecification.header("Authorization", "Bearer ".concat(request.getAuthorizationToken()));
+
+        switch (request.getMethod()) {
+            case GET:
+                response = requestSpecification.when().get(request.getEndpoint()).then().extract().response();
+                Report.logStepInReport(Status.PASS, "<b>[" + action + "]</b> GET request with body: <code>" + gson.toJson(request.getBody()) + "</code>");
+                break;
+            case POST:
+                response = requestSpecification.body(request.getBody()).when().post(request.getEndpoint()).then().extract().response();
+                Report.logStepInReport(Status.PASS, "<b>[" + action + "]</b> POST request with body: <code>" + gson.toJson(request.getBody()) + "</code>");
+                break;
+            case PUT:
+                response = requestSpecification.body(request.getBody()).when().put(request.getEndpoint()).then().extract().response();
+                Report.logStepInReport(Status.PASS, "<b>[" + action + "]</b> PUT request with body: <code>" + gson.toJson(request.getBody()) + "</code>");
+                break;
+            case DELETE:
+                response = requestSpecification.when().delete(request.getEndpoint()).then().extract().response();
+                Report.logStepInReport(Status.PASS, "<b>[" + action + "]</b> DELETE request with body: <code>" + gson.toJson(request.getBody()) + "</code>");
+                break;
+        }
+        Report.logStepInReport(Status.PASS, "<b>[" + action + "]</b> " + request.getMethod() + " response: <code>" + gson.toJson(response.getBody().asString() + "</code>"));
         return response;
     }
 }
